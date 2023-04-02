e8043ce47bb4eee796ae1bbeafa98e9816823434
meherunNmaria <meherun.maria@northsouth.edu>
2023-04-02 11:11:52 +0600

    login js added

---
+++ login.js
@@ -0,0 +1,12 @@
+function validateform(){  
+    var email=document.login.email.value;  
+    var password=document.login.password.value;  
+      
+    if (email==null || email==""){  
+      alert("Email can't be blank");  
+      return false;  
+    }else if(password.length<6){  
+      alert("Password can't be blank.");  
+      return false;  
+      }  
+    }  
